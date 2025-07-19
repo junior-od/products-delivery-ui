c34e08db9f10948639a874808bd9a922a105df93
junior-od <junior.oduberu@leatherback.co>
2025-07-20 00:22:17 +0100

    all bar commponent with animations added

--- app/src/main/java/com/example/ui_take_home_app/ui/components/Bars.kt
+++ app/src/main/java/com/example/ui_take_home_app/ui/components/Bars.kt
@@ -5,7 +5,9 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -42,6 +44,8 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -50,12 +54,14 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -69,6 +75,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -89,23 +97,23 @@ fun BottomBar(
     val navItems = listOf(
         BottomNavItem(
             title = "Home",
-            selectedIcon = Icons.Filled.Home,
-            unselectedIcon = Icons.Outlined.Home
+            selectedIcon = R.drawable.ic_home,
+            unselectedIcon = R.drawable.ic_home
         ),
         BottomNavItem(
             title = "Calculate",
-            selectedIcon = Icons.Filled.Build, // todo find correct icon calculate
-            unselectedIcon = Icons.Outlined.Build
+            selectedIcon = R.drawable.ic_math,
+            unselectedIcon = R.drawable.ic_math
         ),
         BottomNavItem(
             title = "Shipment",
-            selectedIcon = Icons.Filled.Refresh, // todo find correct icon shipping
-            unselectedIcon = Icons.Outlined.Refresh
+            selectedIcon = R.drawable.ic_shipment,
+            unselectedIcon = R.drawable.ic_shipment
         ),
         BottomNavItem(
             title = "Profile",
-            selectedIcon = Icons.Filled.Person,
-            unselectedIcon = Icons.Outlined.Person
+            selectedIcon = R.drawable.ic_profile,
+            unselectedIcon = R.drawable.ic_profile
         )
     )
 
@@ -159,7 +167,8 @@ fun BottomBar(
                         item = item,
                         isSelected = selectedIndex == index,
                         onClick = {
-                            onItemSelected(index) }
+                            onItemSelected(index)
+                        }
                     )
                 }
             }
@@ -187,7 +196,7 @@ fun BottomNavItem(
             .padding(8.dp)
     ) {
         Icon(
-            imageVector = if (isSelected) item.selectedIcon else item.unselectedIcon,
+            painter = painterResource(id = if (isSelected) item.selectedIcon else item.unselectedIcon),
             contentDescription = item.title,
             tint = iconColor,
             modifier = Modifier.size(24.dp)
@@ -206,9 +215,91 @@ fun BottomNavItem(
 // Navigation items data class
 data class BottomNavItem(
     val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector
+    val selectedIcon: Int,
+    val unselectedIcon: Int
 )
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeaderWithFilterTabs(
+    modifier: Modifier = Modifier,
+    title: String,
+    hasFilterTabs: Boolean = false,
+    selectedTabIndex: Int = 0,
+    tabs: List<TabItem> = emptyList(),
+    onBackButtonPressed: () -> Unit = {}
+) {
+    var visible by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) {
+        visible = true
+    }
+
+    Column {
+
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors().copy(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.tertiary,
+                navigationIconContentColor = MaterialTheme.colorScheme.tertiary
+            ),
+            title = {
+                AnimatedVisibility(visible = visible, enter = slideInVertically(
+                    animationSpec = tween(durationMillis = 1100),
+                    initialOffsetY = {
+                        it
+                    }
+                )
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            color = MaterialTheme.colorScheme.tertiary,
+                            fontWeight = if (hasFilterTabs) FontWeight.Bold else FontWeight.Medium
+                        )
+                    )
+                }
+            },
+            navigationIcon = {
+                AnimatedVisibility(
+                    visible = visible, enter = slideInHorizontally(
+                        animationSpec = tween(durationMillis = 1000)
+                    )
+                ) {
+                    IconButton(
+                        onClick = onBackButtonPressed
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.KeyboardArrowLeft,
+                            contentDescription = "Go back",
+                            tint = MaterialTheme.colorScheme.tertiary,
+                            modifier = Modifier.size(64.dp)
+                        )
+
+
+                    }
+                }
+            }
+
+        )
+
+
+        if (hasFilterTabs) {
+            AnimatedVisibility(visible = visible, enter = slideInHorizontally(
+                 animationSpec = tween(durationMillis = 800),
+                 initialOffsetX = {
+                     it / 2
+                 }
+
+                )
+            ) {
+                TabBarWithBadges()
+            }
+
+        }
+
+
+    }
+}
 
 @Composable
 fun TabBarWithBadges() {
@@ -230,8 +321,9 @@ fun TabBarWithBadges() {
         ScrollableTabRow(
             selectedTabIndex = selectedTab,
             containerColor = MaterialTheme.colorScheme.primary, // Purple background
-            contentColor = Color.White,
+            contentColor = MaterialTheme.colorScheme.primary,
             edgePadding = 0.dp,
+            divider = {},
             indicator = { tabPositions ->
                 TabRowDefaults.SecondaryIndicator(
                     Modifier
@@ -310,32 +402,55 @@ fun LocationHeaderComponent(
     onScanClick: () -> Unit = {}
 ) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
-            .animateContentSize()
+    var firstTimeBar by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        firstTimeBar = true
+    }
+
+    AnimatedVisibility(visible = firstTimeBar, enter = slideInVertically(
+        animationSpec = tween(1000)
+       )
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(vertical = 16.dp)
+                .animateContentSize()
+        ) {
 
-        HomeProfileBar(
-            userProfileImage = userProfileImage,
-            location = location,
-            onLocationClick = onLocationClick,
-            onNotificationClick = onNotificationClick,
-            onSearchTextChange = onSearchTextChange,
-            searchText = searchText,
-            onScanClick = onScanClick,
-            onSearchOpen = {
-                // notify others
-            },
-            onSearchClosed = {
-                // notify
-            }
-        )
+            HomeProfileBar(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                userProfileImage = userProfileImage,
+                location = location,
+                onLocationClick = onLocationClick,
+                onNotificationClick = onNotificationClick,
+                onSearchTextChange = onSearchTextChange,
+                searchText = searchText,
+                onScanClick = onScanClick,
+                onSearchOpen = {
+                    // notify others
+                },
+                onSearchClosed = {
+                    // notify
+                }
+            )
+
+//        HeaderWithFilterTabs(
+//            title = "Ship Payemts",
+//            hasFilterTabs = true
+//        )
+
+//        HeaderWithFilterTabs(
+//            title = "Calculate",
+//            hasFilterTabs = false
+//        )
 
 
-
+        }
 
     }
 }
@@ -344,6 +459,7 @@ fun LocationHeaderComponent(
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun HomeProfileBar(
+    modifier: Modifier = Modifier,
     userProfileImage: String = "",
     location: String,
     onNotificationClick: () -> Unit,
@@ -353,13 +469,13 @@ fun HomeProfileBar(
     onScanClick: () -> Unit = {},
     onSearchOpen: () -> Unit = {}, // ← callback when search starts,
     onSearchClosed: () -> Unit = {} // ← callback when search closed
-){
+) {
     var searchValue by remember { mutableStateOf(searchText) }
     var isSearchInProgress by remember {
         mutableStateOf(false)
     }
 
-    Column {
+    Column(modifier = modifier) {
         // animate to hidden when focus is on search
 
         AnimatedVisibility(visible = !isSearchInProgress) {
@@ -378,12 +494,11 @@ fun HomeProfileBar(
                 ) {
                     // Profile image
                     AsyncImage(
-                        model = if (userProfileImage.isNotEmpty()) userProfileImage else R.drawable.ic_launcher_background, // todo change to human image
+                        model = if (userProfileImage.isNotEmpty()) userProfileImage else R.drawable.ic_profile_sample,
                         contentDescription = "Profile picture",
                         modifier = Modifier
                             .size(40.dp)
-                            .clip(CircleShape)
-                            .background(Color.White),
+                            .clip(CircleShape),
                         contentScale = ContentScale.Crop
                     )
 
@@ -395,13 +510,13 @@ fun HomeProfileBar(
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Icon(
-                                imageVector = Icons.Filled.Email, // todo change to map location icon
+                                painter = painterResource(id = R.drawable.ic_location),
                                 contentDescription = "Your location",
                                 tint = lightPurple,
                                 modifier = Modifier.size(16.dp)
                             )
 
-                            Spacer(modifier = Modifier.width(4.dp))
+                            Spacer(modifier = Modifier.width(2.dp))
 
                             Text(
                                 text = "Your location",
@@ -473,27 +588,33 @@ fun HomeProfileBar(
             }
         }
 
-        Row (
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            AnimatedVisibility(visible = isSearchInProgress, enter = slideInHorizontally(
-                animationSpec = spring(
-                    stiffness = Spring.StiffnessLow
+
+            AnimatedVisibility(
+                modifier = Modifier.fillMaxWidth(0.1f),
+                visible = isSearchInProgress,
+                enter = slideInHorizontally(
+                    animationSpec = tween(durationMillis = 1000)
                 )
-            )) {
+            ) {
                 IconButton(onClick = {
+                    onSearchClosed()
+                    isSearchInProgress = false
+
                     focusManager.clearFocus()
                     keyboardController?.hide()
 
-                    onSearchClosed()
-                    isSearchInProgress = false
-                  }
+
+                }
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.KeyboardArrowLeft,
                         contentDescription = "Go back",
                         tint = MaterialTheme.colorScheme.tertiary,
-                        modifier = Modifier.size(24.dp)
+                        modifier = Modifier.size(64.dp)
                     )
 
 
@@ -501,7 +622,9 @@ fun HomeProfileBar(
             }
 
             Card(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateContentSize(),
                 shape = RoundedCornerShape(50.dp),
                 colors = CardDefaults.cardColors(
                     containerColor = Color.White
@@ -590,7 +713,7 @@ fun ScanIcon(
     tint: Color = Color.White
 ) {
     Icon(
-        imageVector = Icons.Default.Info, // todo change to qr scanner
+        painter = painterResource(id = R.drawable.ic_qr),
         contentDescription = "Scan",
         tint = tint,
         modifier = modifier
@@ -604,15 +727,15 @@ data class TabItem(
     val isSelected: Boolean
 )
 
-//@Preview
+@Preview
 @Composable
-fun BottomBarPreview(){
+fun BottomBarPreview() {
     UitakehomeappTheme {
         BottomBar()
     }
 }
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun TabBarPreview() {
     UitakehomeappTheme {
